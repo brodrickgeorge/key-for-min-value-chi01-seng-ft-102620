6ff1b75ba1cad468f6063a5b0d89de5be362f9b8
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 21:24:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,9 @@
 
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
-    min_value = 2
-    if min_value < value
-      min_value = value
-     
+  name_hash.each do |price, item|
+   lowest_price == nil
+   lowest 
     end
   end
 end
