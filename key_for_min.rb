1891f620e39b5aac821263c6e1875d27af7239ef
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 21:31:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |item, price|
-    if lowest_value == nil {price < lowest_value}
+    if lowest_value == nil || price < lowest_value
       lowest_value = price
       lowest_key = item
     end
