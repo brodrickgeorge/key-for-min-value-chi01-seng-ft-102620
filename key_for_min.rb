14f111a5ce1fa08688a4b080ce6a2b656a431a5e
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 21:12:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+
+
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     min_value = 600
     if min_value > value
       min_value = value
     end
-    return min_value
+    return key
   end
 end
